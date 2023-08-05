06745833e14758700a86da045f25afabad1e563e
Eduardo Franco <franc0edu24@gmail.com>
2023-08-05 09:53:18 -0300

    Inicialize project

---
+++ phoneBook_API/Controllers/phoneBook.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace phoneBook_API.Controllers
+{
+    public class phoneBook : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
